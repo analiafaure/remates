7122399cc6fc4ffa41d21949dec0d1ec4434cef2
unknown <analia.f93@gmail.com>
2023-10-02 17:57:36 -0300

    cambios en la carga de datos

--- controllers/datos.js
+++ controllers/datos.js
@@ -41,38 +41,4 @@ exports.cargarLote = async (req, res) => {
       console.error('Error al sincronizar el modelo con la base de datos:', error);
     })
 }
-   /* fs.access(archivoCSV, fs.constants.F_OK, (err) => {
-        if (err) {
-            console.log("entro en el error del archivo");
-          res.writeHead(404, { 'Content-Type': 'text/plain' });
-          res.end('El archivo CSV no existe.');
-        } else {
-            console.log("entra en el else");
-    // Lee el archivo CSV y realiza las inserciones en la base de datos
-    fs.createReadStream(archivoCSV)
-      .pipe(csv())
-      .on('data', (row) => {
-        console.log("row   "+row.partidaInmobiliaria);
-        // Inserta una fila en la base de datos utilizando el modelo de Sequelize
-        Lote.create({
-          partidaInmobiliaria: row.partidaInmobiliaria,
-          descripcion: row.descripcion,
-          costoInicial: row.costoInicial,
-        })
-        .then(() => {
-          console.log('Datos insertados correctamente:', row);
-        })
-        .catch((error) => {
-            console.log("error al insertar  "+ error);
-          console.error('Error al insertar datos:', error);
-        });
-      })
-      .on('end', () => {
-        console.log('Proceso de carga de datos finalizado.');
-        res.status(200).json({
-            ok:true,
-            msg:'Proceso de carga de datos finalizado.'
-        })    
-      });
-    }*/
- 
+   
